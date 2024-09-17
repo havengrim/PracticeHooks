7476eedb653c590a9060887915ce8a31ab10ae98
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-09-17 23:02:49 +0800

    timer day 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './index.css';
-import Day1 from './useState/Day1';
+// import Day1 from './useState/Day1';
+import Day2 from './useState/Day2'
+
 
 function App() {
   return (
     <div className="App container mx-auto p-4">
-      <Day1 />
+      {/*<Day1 />*/}
+      <Day2 />
     </div>
   );
 }
