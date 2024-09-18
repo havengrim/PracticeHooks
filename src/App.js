571d1636db67817b6733996bbd772b4163ef2112
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-09-18 22:12:42 +0800

    counter day 3

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './index.css';
 // import Day1 from './useState/Day1';
 import Day2 from './useState/Day2'
+import Day3 from './useState/Day3';
 
 
 function App() {
   return (
     <div className="App container mx-auto p-4">
       {/*<Day1 />*/}
-      <Day2 />
+      {/*<Day2 />*/}
+      <Day3 />
     </div>
   );
 }
--- src/useState/Day3.jsx
+++ src/useState/Day3.jsx
@@ -1,2 +1,32 @@
 //Put a counter +1 or -1
+import React, {useState} from 'react'
 
+const Day3 = () => {
+    const [counter, setCounter] = useState(0);
+
+    const increase = () => {
+        setCounter(count => count +1 );
+    };
+
+    const decrease = () => {
+        setCounter(count => count > 0 ? count -1 : 0 );
+    };
+
+    const reset = () => {
+        setCounter(0)
+    }
+
+  return (
+    <div>
+        <div className='flex flex-col gap-2'>
+            <h1>This is a sample of react counter</h1>
+            <span>{counter}</span>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    </div>
+  )
+}
+
+export default Day3
