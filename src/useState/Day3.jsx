4bedcd7052b3bc4d0c588475f0a1de3779ef18f3
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-09-22 23:34:04 +0800

    practice git hub hooks counter

--- src/useState/Day3.jsx
+++ src/useState/Day3.jsx
@@ -18,12 +18,13 @@ const Day3 = () => {
 
   return (
     <div>
-        <div className='flex flex-col gap-2'>
-            <h1>This is a sample of react counter</h1>
-            <span>{counter}</span>
-            <button onClick={increase}>+</button>
-            <button onClick={decrease}>-</button>
-            <button onClick={reset}>reset</button>
+        <div className='flex flex-col gap-2 w-full justify-center items-center h-screen'>
+            <div className='rounded-md flex justify-between items-center shadow-md sm:w-1/4 md:w-1/3 lg:w-1/4 xl:w-1/6 w-full h-20'>
+                <button onClick={increase} className='h-full bg-blue-800 p-6 text-white flex justify-center items-center rounded-s-md'>+</button>
+                <span className='text-3xl font-bold'>{counter}</span>
+                <button onClick={decrease} className='h-full bg-blue-800 p-6 text-white flex justify-center items-center rounded-e-md'>-</button>
+            </div>
+            {/* <button onClick={reset}>reset</button> */}
         </div>
     </div>
   )
